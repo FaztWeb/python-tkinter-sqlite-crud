36b83b36b792b490351449628b3d8ae1300b47ce
DiegoArviri <diegoarviri@gmail.com>
2021-10-16 00:08:58 -0300

    modifique index.py

--- index.py
+++ index.py
@@ -133,7 +133,7 @@ class Product:
         parameters = (new_name, new_price,name, old_price)
         self.run_query(query, parameters)
         self.edit_wind.destroy()
-        self.message['text'] = 'Record {} updated successfylly'.format(name)
+        self.message['text'] = 'Record {} updated satisfactoriamente'.format(name)
         self.get_products()
 
 if __name__ == '__main__':
